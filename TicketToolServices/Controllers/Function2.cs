3f810d262114f8bfaa8b9339e65de596e3f351f4
Engelber Amaya <engelberamaya@gmail.com>
2019-09-30 10:51:02 -0400

    changes

--- TicketToolServices/Controllers/Function2.cs
+++ TicketToolServices/Controllers/Function2.cs
@@ -7,11 +7,19 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using TicketToolServices.Models;
+using System.Collections.Generic;
+using static TicketToolServices.Models.Agent;
+using Microsoft.Azure.WebJobs.Host;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
 
 namespace TicketToolServices.Controllers
 {
     public static class Function2
     {
+                
         [FunctionName("Function2")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
