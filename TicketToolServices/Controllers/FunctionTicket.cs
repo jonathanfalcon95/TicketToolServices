97e7f7a06680f7d308e80b6ea46d9b7cc11545a1
jesuselias <jesus_e1992@hotmail.com>
2019-10-01 10:36:21 -0400

    Function ticket with conexion

--- TicketToolServices/Controllers/FunctionTicket.cs
+++ TicketToolServices/Controllers/FunctionTicket.cs
@@ -15,47 +15,21 @@ using TicketToolServices.Repository;
 
 namespace TicketToolServices.Controllers
 {
-    public static class Function1
+    public static class FunctionTickets
     {   
-        //URL t
-        private const string urlTickets = "https://tmconsulting.freshdesk.com/api/v2/tickets";
-        //URL Tickets
-        private const string urlTicketstats = "https://tmconsulting.freshdesk.com/api/v2/tickets";
-
+     
         //Inicio Function
         [FunctionName("Function1")]
         public static void Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
 
         {
 
-            //InstanciaHttpClient de Tickets
-            HttpClient clientTickets = new HttpClient();
-
-            //AsignacionDeURL Tickets
-            clientTickets.BaseAddress = new Uri(urlTickets);
-
-            //Credenciales del API de FreshDesk
-            string yourusername = "UNGq0cadwojfirXm6U7o";
-            string yourpwd = "X";
-
-            //Authorization al API
+            var response = Conexion.GetDataApi("/tickets");
 
 
-            clientTickets.DefaultRequestHeaders.Authorization =
-              new System.Net.Http.Headers.AuthenticationHeaderValue(
-                  "Basic", Convert.ToBase64String(
-                      System.Text.ASCIIEncoding.ASCII.GetBytes(
-                         $"{yourusername}:{yourpwd}")));
-
-
-
-            clientTickets.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-
-            HttpResponseMessage responseTickets = clientTickets.GetAsync(urlTickets).Result;
-            if (responseTickets.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
-                var ticketID = responseTickets.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
+                var ticketID = response.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
                 {
                     foreach (var item in ticketID)
                     {
@@ -65,58 +39,26 @@ namespace TicketToolServices.Controllers
                             TicketID = item.id
 
                         };
-                        //InstanciaHttpClient de Ticketstats
-                        HttpClient clientTicketstats = new HttpClient();
 
-                        //AsignacionDeURL Ticketstats
-                        clientTicketstats.BaseAddress = new Uri(urlTicketstats);
+                        Console.WriteLine(tickets.TicketID);
+                        var response2 = Conexion.GetDataApi("/tickets" + "/" + tickets.TicketID + "?include=stats");
 
-                        clientTicketstats.DefaultRequestHeaders.Authorization =
-                                 new AuthenticationHeaderValue(
-                                     "Basic", Convert.ToBase64String(
-                                         System.Text.ASCIIEncoding.ASCII.GetBytes(
-                                            $"{yourusername}:{yourpwd}")));
-
-                        clientTicketstats.DefaultRequestHeaders.Accept.Add(
-                               new MediaTypeWithQualityHeaderValue("application/json"));
-
-                        HttpResponseMessage responseTicketstats = clientTicketstats.GetAsync(urlTicketstats + "/" + tickets.TicketID + "?include=stats").Result;
-
-                        var dataObjects = responseTicketstats.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
-
-                        foreach (var itemT in dataObjects)
+                        var Data = response2.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
                         {
-                            var Ticketstat = new Tickets()
+                            foreach (var itemT in Data)
                             {
+                                var Datatickets = new Tickets()
+                                {
 
-                                TicketID = itemT.id,
-                                subject = itemT.subject,
-                                description = itemT.description_text
-                                /* status = item.status,
-                                 priority = item.priority,
-                                 source = item.source,
-                                 type = item.type,
-                                 companyID = item.company_id,
-                                 customerID = item.requester_id,
-                                 agentID = item.responder_id,
-                                 groupID = item.group_id,
-                                 creationDate = item.created_at,
-                                 expirationDate = item.due_by,
-                                 lastUpdateDate = item.updated_at,
-                                 resolvedDate = item.custom_fields.cliente*/
-                            };
+                                    description = itemT.description_text
 
+                                };
+                                Console.WriteLine(Datatickets.description);
 
+                            }
                         }
-
-
-
                     }
                 }
-            }
-            else
-            {
-                Console.WriteLine("{0} ({1})", (int)responseTickets.StatusCode, responseTickets.ReasonPhrase);
             }
         }
     }
