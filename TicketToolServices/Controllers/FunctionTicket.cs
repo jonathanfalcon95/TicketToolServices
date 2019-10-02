699df658d92b1e56a15a93ed064e07b6078ab7c8
angelicaadan <angelicaadan06@gmail.com>
2019-10-02 18:08:26 -0400

    Modification of the model, with excel data. Get of ticket

--- TicketToolServices/Controllers/FunctionTicket.cs
+++ TicketToolServices/Controllers/FunctionTicket.cs
@@ -23,10 +23,7 @@ namespace TicketToolServices.Controllers
         public static void Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
 
         {
-
-            var response = Conexion.GetDataApi("/tickets");
-
-
+            var response = Conexion.GetDataApi("/tickets?include=description");
             if (response.IsSuccessStatusCode)
             {
                 var ticketID = response.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
@@ -36,27 +33,85 @@ namespace TicketToolServices.Controllers
                         var tickets = new Tickets()
                         {
 
-                            TicketID = item.id
-
+                            TicketID = item.id,
+                            subject = item.subject,
+                            status = item.status,
+                            priority = item.priority,
+                            source = item.source,
+                            type = item.type,
+                            companyID = item.company_id,
+                            customerID = item.requester_id,
+                            agentID = item.responder_id,
+                            groupID = item.group_id,
+                            creationDate = item.custom_fields.created_at, 
+                            expirationDate = item.due_by, 
+                            lastUpdateDate = item.custom_fields.updated_at,
+                            customerCompany = item.custom_fields.cliente,
+                            projectNumber = item.custom_fields.proyecto,
+                            quotationID = item.custom_fields.id_cotizador,
+                            sharePointID = item.custom_fields.sharepoint_id,
+                            customerEstimatedHours = item.custom_fields.horas_estimadas_por_cliente,
+                            tmHoursWeek1 = item.custom_fields.horas_tampm_semana_1,
+                            tmHoursWeek2 = item.custom_fields.horas_tampm_semana_2,
+                            tmHoursWeek3 = item.custom_fields.horas_tampm_semana_3,
+                            tmHoursWeek4 = item.custom_fields.horas_tampm_semana_4,
+                            progressWeek1 = item.custom_fields.porcentaje_de_avance,
+                            progressWeek2 = item.custom_fields.avance_semana_2,
+                            progressWeek3 = item.custom_fields.avance_semana_3,
+                            progressWeek4 = item.custom_fields.avance_semana_4,
+                            billingMonth = item.custom_fields.mes_facturacin,
+                            //totalBillingHours = item.custom_fields.horas_tampm_semana_1 + item.custom_fields.horas_tampm_semana_2 + item.custom_fields.horas_tampm_semana_3 + item.custom_fields.horas_tampm_semana_4,
+                            // totalProgress = item.custom_fields.porcentaje_de_avance + item.custom_fields.avance_semana_2 + item.custom_fields.avance_semana_3 + item.custom_fields.avance_semana_4,
+                            estimatedStartDate = item.custom_fields.cf_fecha_de_estimada_inicio,
+                            estimatedEndDate = item.custom_fields.cf_fecha_de_estimada_entrega,
+                            realStartDate = item.custom_fields.cf_fecha_de_real_inicio,
+                            realEndDate = item.custom_fields.cf_fecha_de_real_entrega,
+                            estimatedHourAgent= item.custom_fields.cf_horas_estimadas_por_agente
                         };
 
-                        Console.WriteLine(tickets.TicketID);
-                        var response2 = Conexion.GetDataApi("/tickets" + "/" + tickets.TicketID + "?include=stats");
-
-                        var Data = response2.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
+                        Console.WriteLine(tickets.TicketID + " " +
+                                          tickets.customerID + " " +
+                                          tickets.status + " " +
+                                          tickets.tmHoursWeek1 + " " 
+                                         );
+                    }
+                }
+            }
+            //Data that is only in the description url
+            var response2 = Conexion.GetDataApi("/tickets?include=description");
+            if (response2.IsSuccessStatusCode)
+            {
+                var ticketID = response2.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
+                {
+                    foreach (var item in ticketID)
+                    {
+                        var tickets = new Tickets()
                         {
-                            foreach (var itemT in Data)
-                            {
-                                var Datatickets = new Tickets()
-                                {
+                            description = item.description_text,
+                        };
 
-                                    description = itemT.description_text
+                        Console.WriteLine(tickets.description);
 
-                                };
-                                Console.WriteLine(Datatickets.description);
+                    }
+                }
+            }
+            //Data that is only in the description url
+            var response3 = Conexion.GetDataApi("/tickets?include=stats");
+            if (response3.IsSuccessStatusCode)
+            {
+                var ticketID = response3.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
+                {
+                    foreach (var item in ticketID)
+                    {
+                        var tickets = new Tickets()
+                        {
+                            resolvedDate = item.stats.resolved_at,
+                            closedDate = item.stats.closed_at,
+                            fistResponseRequestDate = item.stats.first_responded_at
+                        };
 
-                            }
-                        }
+                        Console.WriteLine(tickets.resolvedDate);
+
                     }
                 }
             }
